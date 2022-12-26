e4cf006d5b571b81f9c4bd0094159c2501ff0c3d
tvpeter <withtvpeter@gmail.com>
2022-12-26 13:16:21 +0100

    add composition method of reusing filereader

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
+//inheritance
+// const reader = new MatchReader('football.csv');
+// reader.read();
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
-/// find number of times man united won
+// create an object that satisfies the 'DataReader' interface
+let csvFileReader = new CsvFileReader('football.csv');
+
+//create an instance of MatchReader and pass in something satisfying the DataReader interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 let manUnitedWins = 0;
-console.log(reader.data);
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
